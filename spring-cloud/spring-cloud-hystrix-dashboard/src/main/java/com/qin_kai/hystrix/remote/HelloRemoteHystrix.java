0a68a7be28ebb561a89d618ba4f18feab5798fe8
qinkai <qinkkai@foxmail.com>
2020-09-04 16:59:58 +0800

    添加hystrix dashboard demo

---
+++ spring-cloud/spring-cloud-hystrix-dashboard/src/main/java/com/qin_kai/hystrix/remote/HelloRemoteHystrix.java
@@ -0,0 +1,15 @@
+package com.qin_kai.hystrix.remote;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author qin kai
+ * @Date 2020/9/4
+ */
+@Component
+public class HelloRemoteHystrix implements HelloRemote {
+    @Override
+    public String hello(String name) {
+        return "sorry " + name + " some error happened";
+    }
+}
