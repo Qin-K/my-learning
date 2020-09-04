3dcd6fb2745adb141b2c7f1154d5575e0c9081b4
qinkai <qinkkai@foxmail.com>
2020-09-04 14:30:36 +0800

    添加使用gradle构建spring-boot项目demo

---
+++ spring-boot/spring-boot-gradle/src/main/java/com/qin_kai/gradle/GradleApplication.java
@@ -0,0 +1,13 @@
+package com.qin_kai.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleApplication.class, args);
+    }
+
+}
---
+++ spring-boot/spring-boot-gradle/src/test/java/com/qin_kai/gradle/SpringBootGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qin_kai.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
