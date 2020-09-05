9eb157325336cb22a745caf2d7306dc8cf7a6cc9
qinkai <qinkkai@foxmail.com>
2020-09-05 15:58:18 +0800

    添加spring cloud config svn demo

---
+++ spring-cloud/spring-cloud-config-svn/spring-cloud-config-server/src/test/java/com/qin_kai/config/svn/ConfigSvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qin_kai.config.svn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigSvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
