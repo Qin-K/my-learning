224927eb7e1413a440e8934fde2651bf4c428c13
qinkai <qinkkai@foxmail.com>
2020-09-05 13:10:43 +0800

    添加spring cloud config结合git实现配置中心

---
+++ spring-cloud/spring-cloud-config-git/spring-cloud-config-server/src/test/java/com/qin_kai/config/git/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qin_kai.config.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
