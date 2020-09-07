d3e8871d55969ff4ffbdd143094bce88f42ee6ba
qinkai <qinkkai@foxmail.com>
2020-09-07 14:11:04 +0800

    修改服务注册发现使用EnableDiscoveryClient注解

--- spring-cloud/spring-cloud-hystrix-dashboard/src/main/java/com/qin_kai/hystrix/HystrixDashboardApplication.java
+++ spring-cloud/spring-cloud-hystrix-dashboard/src/main/java/com/qin_kai/hystrix/HystrixDashboardApplication.java
@@ -3,12 +3,12 @@ package com.qin_kai.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrixDashboard
 @EnableCircuitBreaker
--- spring-cloud/spring-cloud-producer-consumer/spring-cloud-eureka-producer/src/main/java/com/qin_kai/producer/ProducerApplication.java
+++ spring-cloud/spring-cloud-producer-consumer/spring-cloud-eureka-producer/src/main/java/com/qin_kai/producer/ProducerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ProducerApplication {
 
     public static void main(String[] args) {
