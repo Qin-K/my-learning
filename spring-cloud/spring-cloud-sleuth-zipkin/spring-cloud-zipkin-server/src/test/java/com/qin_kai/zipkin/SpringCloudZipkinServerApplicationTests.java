1ca1f8e062ebb9292ffafdfa9aded950e9385f81
qinkai <qinkkai@foxmail.com>
2020-09-10 18:19:47 +0800

    添加zipkin sleuth demo

---
+++ spring-cloud/spring-cloud-sleuth-zipkin/spring-cloud-zipkin-server/src/test/java/com/qin_kai/zipkin/SpringCloudZipkinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qin_kai.zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudZipkinServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
